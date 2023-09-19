8aa404115811ccb119c86a3ba3bf3ef172f8d2d7
tmurilloa <tmurilloa@unal.edu.co>
2023-09-19 11:14:52 -0500

    Update Animal.java

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -32,7 +32,7 @@ public class Animal {
 	} 
 	
 	public static String totalPorTipo() {
-		String resultado = "Mamiferos: " + Mamifero.cantidadMamiferos() +
+		String resultado = "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" +
 				"Aves: " + Ave.cantidadAves() + "\n" +
 				"Reptiles:" + Reptil.cantidadReptiles() + "\n" +
 				"Peces: " + Pez.cantidadPeces() + "\n" +
